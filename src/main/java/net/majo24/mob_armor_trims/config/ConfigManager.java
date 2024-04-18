2d6d5cd3fecf09eeb92297328092d511de5bc7b2
Imajo24I <145028855+Imajo24I@users.noreply.github.com>
2024-04-18 13:24:10 +0200

    add Default Armor Trim Chance
    use and add DefaultTrimChance instead of 50 everywhere

--- src/main/java/net/majo24/mob_armor_trims/config/ConfigManager.java
+++ src/main/java/net/majo24/mob_armor_trims/config/ConfigManager.java
@@ -11,6 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class ConfigManager {
+    public static final int DEFAULT_TRIM_CHANCE = 50;
     private final Config config;
     public static final Path configPath = FabricLoader.getInstance().getConfigDir().resolve(MobArmorTrims.MOD_ID + ".json");
     public static final Gson GSON = new GsonBuilder().setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_DASHES)
@@ -23,7 +24,7 @@ public class ConfigManager {
 
     public static Config getConfigFromFile() {
         if (!Files.exists(configPath)) {
-            Config newConfig = new Config(50);
+            Config newConfig = new Config(DEFAULT_TRIM_CHANCE);
             try {
                 MobArmorTrims.LOGGER.info("Creating config file for Mob Armor Trims");
                 Files.createFile(configPath);
@@ -41,7 +42,7 @@ public class ConfigManager {
                 return GSON.fromJson(jsonConfig, Config.class);
             } catch (IOException e) {
                 MobArmorTrims.LOGGER.error("Could not read config file", e);
-                return new Config(50);
+                return new Config(DEFAULT_TRIM_CHANCE);
             }
         }
     }
