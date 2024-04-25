537368ce33acdc52fc70d290e0aa311fa5802f93
Martin <martin.j.schilling@gmx.de>
2024-04-25 18:39:49 +0200

    Refactor Config Stuff

--- src/main/java/net/majo24/mob_armor_trims/config/Config.java
+++ src/main/java/net/majo24/mob_armor_trims/config/Config.java
@@ -1,9 +1,17 @@
 package net.majo24.mob_armor_trims.config;
 
 public class Config {
-    public int trimChance = 50;
+    private int trimChance = 50;
 
     public Config(int trimChanceToSet) {
         trimChance = trimChanceToSet;
+    }
+
+    public int getTrimChance() {
+        return trimChance;
+    }
+
+    public void setTrimChance(int trimChance) {
+        this.trimChance = trimChance;
     }
 }
