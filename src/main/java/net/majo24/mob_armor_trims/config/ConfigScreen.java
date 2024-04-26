6bbbe805833dd4acf486925f1a59dc5c673a1f26
Martin <martin.j.schilling@gmx.de>
2024-04-26 21:15:25 +0200

    Improve Readability of Tooltip

--- src/main/java/net/majo24/mob_armor_trims/config/ConfigScreen.java
+++ src/main/java/net/majo24/mob_armor_trims/config/ConfigScreen.java
@@ -11,9 +11,9 @@ public class ConfigScreen {
     private ConfigScreen() {}
 
     public static Screen getConfigScreen(Screen parent) {
-           ConfigBuilder builder = ConfigBuilder.create()
-        .setParentScreen(parent)
-        .setTitle(Text.literal("Mob Armor Trims"));
+       ConfigBuilder builder = ConfigBuilder.create()
+            .setParentScreen(parent)
+            .setTitle(Text.literal("Mob Armor Trims"));
         builder.setSavingRunnable(MobArmorTrims.configManager::saveConfig);
 
         ConfigCategory general = builder.getOrCreateCategory(Text.literal("General"));
@@ -21,7 +21,7 @@ public class ConfigScreen {
 
         general.addEntry(entryBuilder.startIntSlider(Text.literal("Trim Chance"), MobArmorTrims.configManager.getTrimChance(), 0, 100)
                         .setDefaultValue(ConfigManager.DEFAULT_TRIM_CHANCE)
-                        .setTooltip(Text.literal("Chance of each armor piece of a mob having an armor trim"))
+                        .setTooltip(Text.literal("Chance of each armor piece\nof a mob having an armor trim"))
                         .setSaveConsumer(MobArmorTrims.configManager::setTrimChance)
                         .build());
         return builder.build();
