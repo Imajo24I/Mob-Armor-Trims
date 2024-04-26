39703a2909783bf10cd65dd1972de8cbe4e0e540
Imajo24I <145028855+Imajo24I@users.noreply.github.com>
2024-04-26 18:23:49 +0200

    remove unneccessary empty lines

--- src/main/java/net/majo24/mob_armor_trims/config/ConfigManager.java
+++ src/main/java/net/majo24/mob_armor_trims/config/ConfigManager.java
@@ -50,9 +50,6 @@ public class ConfigManager {
         }
     }
 
-
-
-
     public void saveConfig() {
         MobArmorTrims.LOGGER.info("Saving config file");
         String jsonConfig = GSON.toJson(config);
