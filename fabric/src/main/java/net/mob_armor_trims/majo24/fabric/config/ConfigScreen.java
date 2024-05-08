fd2074f9f289bc883127b12b0f2e53e434daf533
Martin <martin.j.schilling@gmx.de>
2024-05-08 17:43:09 +0200

    config screen + mod menu integration

---
+++ fabric/src/main/java/net/mob_armor_trims/majo24/fabric/config/ConfigScreen.java
@@ -0,0 +1,45 @@
+package net.mob_armor_trims.majo24.fabric.config;
+
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+import net.mob_armor_trims.majo24.MobArmorTrims;
+import net.mob_armor_trims.majo24.config.ConfigManager;
+
+public class ConfigScreen {
+    private ConfigScreen() {}
+
+    public static Screen getConfigScreen(Screen parent) {
+           ConfigBuilder builder = ConfigBuilder.create()
+        .setParentScreen(parent)
+        .setTitle(Component.literal("Mob Armor Trims"));
+        builder.setSavingRunnable(MobArmorTrims.configManager::saveConfig);
+
+        ConfigCategory general = builder.getOrCreateCategory(Component.literal("General"));
+        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+
+        general.addEntry(entryBuilder.startIntSlider(Component.literal("Trim Chance"), MobArmorTrims.configManager.getTrimChance(), 0, 100)
+                        .setDefaultValue(ConfigManager.DEFAULT_TRIM_CHANCE)
+                        .setTooltip(Component.literal("Chance of each armor piece\nof a mob having an armor trim"))
+                        .setSaveConsumer(MobArmorTrims.configManager::setTrimChance)
+                        .build());
+
+        if (MobArmorTrims.isStackedArmorTrimsLoaded) {
+            general.addEntry(entryBuilder.startIntSlider(Component.literal("Stacked Trim Chance"), MobArmorTrims.configManager.getStackedTrimChance(), 0, 100)
+                            .setDefaultValue(ConfigManager.DEFAULT_STACKED_TRIM_CHANCE)
+                            .setTooltip(Component.literal("Chance of each armor piece having an additional armor trim on it\nwhen the Stacked Armor Trims mod is enabled"))
+                            .setSaveConsumer(MobArmorTrims.configManager::setStackedTrimChance)
+                            .build());
+
+            general.addEntry(entryBuilder.startIntSlider(Component.literal("Max Stacked Trims"), MobArmorTrims.configManager.getMaxStackedTrims(), 0, 100)
+                            .setDefaultValue(ConfigManager.DEFAULT_MAX_STACKED_TRIMS)
+                            .setTooltip(Component.literal("The maximum amount of armor trims that can be stacked on each other\nwhen the Stacked Armor Trims mod is enabled"))
+                            .setSaveConsumer(MobArmorTrims.configManager::setMaxStackedTrims)
+                            .build());
+        }
+
+        return builder.build();
+    }
+}
---
+++ fabric/src/main/java/net/mob_armor_trims/majo24/fabric/config/ModMenuIntegration.java
@@ -0,0 +1,11 @@
+package net.mob_armor_trims.majo24.fabric.config;
+
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+
+public class ModMenuIntegration implements ModMenuApi {
+    @Override
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return ConfigScreen::getConfigScreen;
+    }
+}
