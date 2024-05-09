4cca6bdbfdbda276bb7ca4826b91e75a2b636f2c
Martin <martin.j.schilling@gmx.de>
2024-05-09 12:17:48 +0200

    remove unused imports

--- common/src/main/java/net/mob_armor_trims/majo24/MobArmorTrims.java
+++ common/src/main/java/net/mob_armor_trims/majo24/MobArmorTrims.java
@@ -1,16 +1,5 @@
 package net.mob_armor_trims.majo24;
 
-import net.minecraft.core.Holder;
-import net.minecraft.core.Registry;
-import net.minecraft.core.RegistryAccess;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.armortrim.ArmorTrim;
-import net.minecraft.world.item.armortrim.TrimMaterial;
-import net.minecraft.world.item.armortrim.TrimPattern;
 import net.mob_armor_trims.majo24.config.ConfigManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
