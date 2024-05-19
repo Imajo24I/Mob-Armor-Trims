de79004eb5714f7c4fd608c53660bb4bbf4174fa
Martin <martin.j.schilling@gmx.de>
2024-05-19 20:12:14 +0200

    fix warning

--- common/src/main/java/net/mob_armor_trims/majo24/mixin/spawn_entities_trimmed/OnNaturalSpawnMixin.java
+++ common/src/main/java/net/mob_armor_trims/majo24/mixin/spawn_entities_trimmed/OnNaturalSpawnMixin.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.Mob;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.mob_armor_trims.majo24.RandomTrims;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +17,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 
 @Mixin(Mob.class)
 public abstract class OnNaturalSpawnMixin extends LivingEntity {
-    @Shadow public abstract Iterable<ItemStack> getArmorSlots();
+    @Shadow public abstract @NotNull Iterable<ItemStack> getArmorSlots();
 
     @Inject(at = @At("TAIL"), method = "populateDefaultEquipmentSlots")
     private void populateDefaultEquipmentSlots(RandomSource randomSource, DifficultyInstance difficultyInstance, CallbackInfo ci) {
