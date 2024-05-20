13ebb825f4d52a760487f86d58aae540c85594f4
Martin <martin.j.schilling@gmx.de>
2024-05-20 12:24:08 +0200

    remove architectury api dependency

--- forge/src/main/java/net/mob_armor_trims/majo24/forge/MobArmorTrimsForge.java
+++ forge/src/main/java/net/mob_armor_trims/majo24/forge/MobArmorTrimsForge.java
@@ -1,11 +1,9 @@
 package net.mob_armor_trims.majo24.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import net.minecraftforge.fml.loading.FMLPaths;
 import net.mob_armor_trims.majo24.MobArmorTrims;
@@ -14,9 +12,6 @@ import net.mob_armor_trims.majo24.forge.config.ConfigScreen;
 @Mod(MobArmorTrims.MOD_ID)
 public final class MobArmorTrimsForge {
     public MobArmorTrimsForge() {
-        // Submit our event bus to let Architectury API register our content on the right time.
-        EventBuses.registerModEventBus(MobArmorTrims.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-
         MobArmorTrims.isStackedArmorTrimsLoaded = ModList.get().isLoaded("stacked_trims");
 
         // Register Config Screen
@@ -24,8 +19,6 @@ public final class MobArmorTrimsForge {
             ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
                     () -> new ConfigScreenHandler.ConfigScreenFactory((client, parent) -> ConfigScreen.getConfigScreen(parent)));
         }
-
-        //fixme: currently architecutry api is still a dependency. Test when working, if its possible to remove, because of line 18
 
         // Run our common setup.
         MobArmorTrims.init(FMLPaths.CONFIGDIR.get().resolve("mob_armor_trims.json"));
