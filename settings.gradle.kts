c60e4090be4ac67ab93cd99d3528b6874605a62a
Martin <martin.j.schilling@gmx.de>
2024-06-14 13:26:30 +0200

    Update Stonecutter

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.kikugie.stonecutter") version "0.4-beta.3"
+    id("dev.kikugie.stonecutter") version "0.4-rc.2"
 }
 
 extensions.configure<StonecutterSettings> {
