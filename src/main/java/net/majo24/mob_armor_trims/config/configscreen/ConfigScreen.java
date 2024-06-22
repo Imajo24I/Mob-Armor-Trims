23b5da4144329d530ea27d75e8edf8cb72654626
Martin <martin.j.schilling@gmx.de>
2024-06-22 16:08:31 +0200

    fix indentation

--- src/main/java/net/majo24/mob_armor_trims/config/configscreen/ConfigScreen.java
+++ src/main/java/net/majo24/mob_armor_trims/config/configscreen/ConfigScreen.java
@@ -53,122 +53,122 @@ public class ConfigScreen {
 
     private static ConfigCategory buildGeneralCategory() {
         return ConfigCategory.createBuilder()
-            .name(translatable("mob_armor_trims.config.general"))
-            .tooltip(translatable("mob_armor_trims.config.general.tooltip"))
-            .option(Option.<Config.TrimSystem>createBuilder()
-                .name(translatable("mob_armor_trims.config.general.trimSystem"))
-                .description(OptionDescription.of(translatable("mob_armor_trims.config.general.trimSystem.description")))
-                .binding(Config.TrimSystem.RANDOM_TRIMS,
-                        () -> MobArmorTrims.configManager.getEnabledSystem(),
-                        enabledSystem -> MobArmorTrims.configManager.setEnabledSystem(enabledSystem))
-                .controller(opt -> EnumControllerBuilder.create(opt)
-                        .enumClass(Config.TrimSystem.class)
-                        .formatValue(trimSystemFormatter))
-                .build())
+                .name(translatable("mob_armor_trims.config.general"))
+                .tooltip(translatable("mob_armor_trims.config.general.tooltip"))
+                .option(Option.<Config.TrimSystem>createBuilder()
+                        .name(translatable("mob_armor_trims.config.general.trimSystem"))
+                        .description(OptionDescription.of(translatable("mob_armor_trims.config.general.trimSystem.description")))
+                        .binding(Config.TrimSystem.RANDOM_TRIMS,
+                                () -> MobArmorTrims.configManager.getEnabledSystem(),
+                                enabledSystem -> MobArmorTrims.configManager.setEnabledSystem(enabledSystem))
+                        .controller(opt -> EnumControllerBuilder.create(opt)
+                                .enumClass(Config.TrimSystem.class)
+                                .formatValue(trimSystemFormatter))
+                        .build())
 
-            .option(Option.<Integer>createBuilder()
-                .name(translatable("mob_armor_trims.config.general.noTrimsChance"))
-                .description(OptionDescription.of(translatable("mob_armor_trims.config.general.noTrimsChance.description")))
-                .binding(ConfigManager.DEFAULT_NO_TRIMS_CHANCE,
-                        () -> MobArmorTrims.configManager.getNoTrimsChance(),
-                        noTrimsChance -> MobArmorTrims.configManager.setNoTrimsChance(noTrimsChance))
-                .controller(opt -> IntegerSliderControllerBuilder.create(opt)
-                        .range(0, 100)
-                        .step(1)
-                        .formatValue(integerToPercentageFormatter))
-                .build())
-            .build();
+                .option(Option.<Integer>createBuilder()
+                        .name(translatable("mob_armor_trims.config.general.noTrimsChance"))
+                        .description(OptionDescription.of(translatable("mob_armor_trims.config.general.noTrimsChance.description")))
+                        .binding(ConfigManager.DEFAULT_NO_TRIMS_CHANCE,
+                                () -> MobArmorTrims.configManager.getNoTrimsChance(),
+                                noTrimsChance -> MobArmorTrims.configManager.setNoTrimsChance(noTrimsChance))
+                        .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                .range(0, 100)
+                                .step(1)
+                                .formatValue(integerToPercentageFormatter))
+                        .build())
+                .build();
     }
 
     private static ConfigCategory buildRandomTrimsCategory() {
         return ConfigCategory.createBuilder()
-            .name(translatable("mob_armor_trims.config.randomTrims"))
-            .tooltip(translatable("mob_armor_trims.config.randomTrims.tooltip"))
+                .name(translatable("mob_armor_trims.config.randomTrims"))
+                .tooltip(translatable("mob_armor_trims.config.randomTrims.tooltip"))
 
-            .option(Option.<Integer>createBuilder()
-                .name(translatable("mob_armor_trims.config.randomTrims.trimChance"))
-                .description(OptionDescription.of(translatable("mob_armor_trims.config.randomTrims.trimChance.description")))
-                .binding(ConfigManager.DEFAULT_TRIM_CHANCE,
-                        () -> MobArmorTrims.configManager.getTrimChance(),
-                        trimsChance -> MobArmorTrims.configManager.setTrimChance(trimsChance))
-                .controller(opt -> IntegerSliderControllerBuilder.create(opt)
-                        .range(0, 100)
-                        .step(1)
-                        .formatValue(integerToPercentageFormatter))
-                .build())
+                .option(Option.<Integer>createBuilder()
+                        .name(translatable("mob_armor_trims.config.randomTrims.trimChance"))
+                        .description(OptionDescription.of(translatable("mob_armor_trims.config.randomTrims.trimChance.description")))
+                        .binding(ConfigManager.DEFAULT_TRIM_CHANCE,
+                                () -> MobArmorTrims.configManager.getTrimChance(),
+                                trimsChance -> MobArmorTrims.configManager.setTrimChance(trimsChance))
+                        .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                .range(0, 100)
+                                .step(1)
+                                .formatValue(integerToPercentageFormatter))
+                        .build())
 
-            .option(Option.<Integer>createBuilder()
-                .name(translatable("mob_armor_trims.config.randomTrims.similarTrimChance"))
-                .description(OptionDescription.of(translatable("mob_armor_trims.config.randomTrims.similarTrimChance.description")))
-                .binding(ConfigManager.DEFAULT_SIMILAR_TRIM_CHANCE,
-                        () -> MobArmorTrims.configManager.getSimilarTrimChance(),
-                        similarTrimChance -> MobArmorTrims.configManager.setSimilarTrimChance(similarTrimChance))
-                .controller(opt -> IntegerSliderControllerBuilder.create(opt)
-                        .range(0, 100)
-                        .step(1)
-                        .formatValue(integerToPercentageFormatter))
-                .build())
+                .option(Option.<Integer>createBuilder()
+                        .name(translatable("mob_armor_trims.config.randomTrims.similarTrimChance"))
+                        .description(OptionDescription.of(translatable("mob_armor_trims.config.randomTrims.similarTrimChance.description")))
+                        .binding(ConfigManager.DEFAULT_SIMILAR_TRIM_CHANCE,
+                                () -> MobArmorTrims.configManager.getSimilarTrimChance(),
+                                similarTrimChance -> MobArmorTrims.configManager.setSimilarTrimChance(similarTrimChance))
+                        .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                .range(0, 100)
+                                .step(1)
+                                .formatValue(integerToPercentageFormatter))
+                        .build())
 
-            .build();
+                .build();
     }
 
     private static ConfigCategory buildCustomTrimsCategory() {
         return ConfigCategory.createBuilder()
-            .name(translatable("mob_armor_trims.config.customTrims"))
-            .tooltip(translatable("mob_armor_trims.config.customTrims.tooltip"))
+                .name(translatable("mob_armor_trims.config.customTrims"))
+                .tooltip(translatable("mob_armor_trims.config.customTrims.tooltip"))
 
-            .group(ListOption.<Config.CustomTrim>createBuilder()
-                .name(translatable("mob_armor_trims.config.customTrims.customTrimsList"))
-                .description(OptionDescription.of(translatable("mob_armor_trims.config.customTrims.customTrimsList.description")))
-                .binding(new ArrayList<>(),
-                        () -> MobArmorTrims.configManager.getCustomTrimsList(),
-                        customTrimsList -> MobArmorTrims.configManager.setCustomTrimsList(customTrimsList))
-                .controller(opt -> CustomTrimsListController.Builder.create(opt)
-                        .patternController(StringControllerBuilder::create)
-                        .materialController(StringControllerBuilder::create))
-                .initial(new Config.CustomTrim("", ""))
-                .build())
+                .group(ListOption.<Config.CustomTrim>createBuilder()
+                        .name(translatable("mob_armor_trims.config.customTrims.customTrimsList"))
+                        .description(OptionDescription.of(translatable("mob_armor_trims.config.customTrims.customTrimsList.description")))
+                        .binding(new ArrayList<>(),
+                                () -> MobArmorTrims.configManager.getCustomTrimsList(),
+                                customTrimsList -> MobArmorTrims.configManager.setCustomTrimsList(customTrimsList))
+                        .controller(opt -> CustomTrimsListController.Builder.create(opt)
+                                .patternController(StringControllerBuilder::create)
+                                .materialController(StringControllerBuilder::create))
+                        .initial(new Config.CustomTrim("", ""))
+                        .build())
 
-            .option(Option.<Boolean>createBuilder()
-                    .name(translatable("mob_armor_trims.config.customTrims.applyToEntireArmor"))
-                    .description(OptionDescription.of(translatable("mob_armor_trims.config.customTrims.applyToEntireArmor.description")))
-                    .binding(ConfigManager.DEFAULT_APPLY_TO_ENTIRE_ARMOR,
-                            () -> MobArmorTrims.configManager.getApplyToEntireArmor(),
-                            applyToEntireArmor -> MobArmorTrims.configManager.setApplyToEntireArmor(applyToEntireArmor))
-                    .controller(BooleanControllerBuilder::create)
-                    .build())
+                .option(Option.<Boolean>createBuilder()
+                        .name(translatable("mob_armor_trims.config.customTrims.applyToEntireArmor"))
+                        .description(OptionDescription.of(translatable("mob_armor_trims.config.customTrims.applyToEntireArmor.description")))
+                        .binding(ConfigManager.DEFAULT_APPLY_TO_ENTIRE_ARMOR,
+                                () -> MobArmorTrims.configManager.getApplyToEntireArmor(),
+                                applyToEntireArmor -> MobArmorTrims.configManager.setApplyToEntireArmor(applyToEntireArmor))
+                        .controller(BooleanControllerBuilder::create)
+                        .build())
 
-            .build();
+                .build();
     }
 
     private static ConfigCategory buildStackedTrimsCategory() {
         return ConfigCategory.createBuilder()
-            .name(translatable("mob_armor_trims.config.stackedTrims"))
-            .tooltip(translatable("mob_armor_trims.config.stackedTrims.tooltip"))
+                .name(translatable("mob_armor_trims.config.stackedTrims"))
+                .tooltip(translatable("mob_armor_trims.config.stackedTrims.tooltip"))
 
-            .option(Option.<Integer>createBuilder()
-                .name(translatable("mob_armor_trims.config.stackedTrims.stackedTrimsChance"))
-                .description(OptionDescription.of(translatable("mob_armor_trims.config.stackedTrims.stackedTrimsChance.description")))
-                .binding(ConfigManager.DEFAULT_STACKED_TRIM_CHANCE,
-                        () -> MobArmorTrims.configManager.getStackedTrimChance(),
-                        stackedTrimChance -> MobArmorTrims.configManager.setStackedTrimChance(stackedTrimChance))
-                .controller(opt -> IntegerSliderControllerBuilder.create(opt)
-                    .range(0, 100)
-                    .step(1)
-                    .formatValue(integerToPercentageFormatter))
-                .build())
+                .option(Option.<Integer>createBuilder()
+                        .name(translatable("mob_armor_trims.config.stackedTrims.stackedTrimsChance"))
+                        .description(OptionDescription.of(translatable("mob_armor_trims.config.stackedTrims.stackedTrimsChance.description")))
+                        .binding(ConfigManager.DEFAULT_STACKED_TRIM_CHANCE,
+                                () -> MobArmorTrims.configManager.getStackedTrimChance(),
+                                stackedTrimChance -> MobArmorTrims.configManager.setStackedTrimChance(stackedTrimChance))
+                        .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                .range(0, 100)
+                                .step(1)
+                                .formatValue(integerToPercentageFormatter))
+                        .build())
 
-            .option(Option.<Integer>createBuilder()
-                .name(translatable("mob_armor_trims.config.stackedTrims.maxStackedTrims"))
-                .description(OptionDescription.of(translatable("mob_armor_trims.config.stackedTrims.maxStackedTrims.description")))
-                .binding(ConfigManager.DEFAULT_MAX_STACKED_TRIMS,
-                        () -> MobArmorTrims.configManager.getMaxStackedTrims(),
-                        maxStackedTrims -> MobArmorTrims.configManager.setMaxStackedTrims(maxStackedTrims))
-                .controller(opt -> IntegerSliderControllerBuilder.create(opt)
-                    .range(0, 5)
-                    .step(1))
-                .build())
-            .build();
+                .option(Option.<Integer>createBuilder()
+                        .name(translatable("mob_armor_trims.config.stackedTrims.maxStackedTrims"))
+                        .description(OptionDescription.of(translatable("mob_armor_trims.config.stackedTrims.maxStackedTrims.description")))
+                        .binding(ConfigManager.DEFAULT_MAX_STACKED_TRIMS,
+                                () -> MobArmorTrims.configManager.getMaxStackedTrims(),
+                                maxStackedTrims -> MobArmorTrims.configManager.setMaxStackedTrims(maxStackedTrims))
+                        .controller(opt -> IntegerSliderControllerBuilder.create(opt)
+                                .range(0, 5)
+                                .step(1))
+                        .build())
+                .build();
     }
 
     static class BackupScreen extends OptionsSubScreen {
