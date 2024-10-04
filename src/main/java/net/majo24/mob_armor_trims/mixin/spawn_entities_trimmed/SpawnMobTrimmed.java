ace5e4cb6ff08fba4eb827cc54424da2b9d41a71
Martin <martin.j.schilling@gmx.de>
2024-10-04 14:45:30 +0200

    cleanup

--- src/main/java/net/majo24/mob_armor_trims/mixin/spawn_entities_trimmed/SpawnMobTrimmed.java
+++ src/main/java/net/majo24/mob_armor_trims/mixin/spawn_entities_trimmed/SpawnMobTrimmed.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.Inject;
 
-@Mixin(value = Mob.class)
+@Mixin(Mob.class)
 public abstract class SpawnMobTrimmed extends LivingEntity {
     protected SpawnMobTrimmed(EntityType<? extends LivingEntity> entityType, Level world) {
         super(entityType, world);
