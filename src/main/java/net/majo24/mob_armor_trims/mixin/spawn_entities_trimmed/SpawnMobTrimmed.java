7dda9c64d7daf7df6e399f5745dd172ae0de4daa
Martin <martin.j.schilling@gmx.de>
2024-10-04 14:44:55 +0200

    cleanup

--- src/main/java/net/majo24/mob_armor_trims/mixin/spawn_entities_trimmed/SpawnMobTrimmed.java
+++ src/main/java/net/majo24/mob_armor_trims/mixin/spawn_entities_trimmed/SpawnMobTrimmed.java
@@ -6,26 +6,20 @@ import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.Inject;
 
-@Mixin(Mob.class)
-public abstract class EntityArmorEquipMixin extends LivingEntity {
-    @Shadow public abstract @NotNull Iterable<ItemStack> getArmorSlots();
+@Mixin(value = Mob.class)
+public abstract class SpawnMobTrimmed extends LivingEntity {
+    protected SpawnMobTrimmed(EntityType<? extends LivingEntity> entityType, Level world) {
+        super(entityType, world);
+    }
 
     @Inject(at = @At("TAIL"), method = "populateDefaultEquipmentSlots")
     private void populateDefaultEquipmentSlots(RandomSource randomSource, DifficultyInstance difficultyInstance, CallbackInfo ci) {
-
         TrimApplier.applyTrims(this.level().registryAccess(), super.random, this.getArmorSlots());
-    }
-
-    protected EntityArmorEquipMixin(EntityType<? extends LivingEntity> entityType, Level world) {
-        super(entityType, world);
     }
 }
