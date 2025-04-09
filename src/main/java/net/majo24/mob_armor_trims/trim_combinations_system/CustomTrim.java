6b71ed0b19172cdf2cae37a208379add8045b399
Martin <martin.j.schilling@gmx.de>
2025-04-09 16:11:28 +0200

    cleanup and remove unused methods

--- src/main/java/net/majo24/mob_armor_trims/trim_combinations_system/CustomTrim.java
+++ src/main/java/net/majo24/mob_armor_trims/trim_combinations_system/CustomTrim.java
@@ -4,7 +4,6 @@ import net.minecraft.core.Holder;
 import net.minecraft.core.RegistryAccess;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 
