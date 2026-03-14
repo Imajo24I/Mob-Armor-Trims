e262c5810266c56b5c2d08209a729e2aef88a56c
Martin <martin.j.schilling@gmx.de>
2026-03-14 21:49:16 +0100

    progress on 1.20.1

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -3,5 +3,6 @@ plugins {
     id("me.modmuss50.mod-publish-plugin") version "0.5.1"
     id("fabric-loom") version "1.13-SNAPSHOT" apply false
     id("net.neoforged.moddev") version "2.0.140" apply false
+    id("net.minecraftforge.gradle") version "7.0.15" apply false
 }
 stonecutter active "1.21.11-fabric" /* [SC] DO NOT EDIT */
