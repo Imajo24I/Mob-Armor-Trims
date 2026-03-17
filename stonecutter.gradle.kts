1925b1c5815f4d380476523c039f1e51ddbc9517
Martin <martin.j.schilling@gmx.de>
2026-03-17 16:40:09 +0100

    Update Loom

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("dev.kikugie.stonecutter")
     id("me.modmuss50.mod-publish-plugin") version "0.5.1"
-    id("fabric-loom") version "1.13-SNAPSHOT" apply false
+    id("fabric-loom") version "1.15-SNAPSHOT" apply false
     id("net.neoforged.moddev") version "2.0.140" apply false
     id("net.minecraftforge.gradle") version "7.0.15" apply false
 }
