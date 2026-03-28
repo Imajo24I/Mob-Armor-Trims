42bc72ae3656eab151b30a78bcc257aa3e575221
Martin <martin.j.schilling@gmx.de>
2026-03-28 11:15:55 +0100

    fix 1.20.6 and below not compiling

--- src/main/java/net/majo24/naturally_trimmed/config/screen/ConfigScreen.java
+++ src/main/java/net/majo24/naturally_trimmed/config/screen/ConfigScreen.java
@@ -236,6 +236,9 @@ public class ConfigScreen {
                         .name((translatable("naturally_trimmed.config.utils.openFile")))
                         .description(OptionDescription.of(translatable("naturally_trimmed.config.utils.openFile")))
                         .text(translatable("naturally_trimmed.config.utils.run"))
+                        //? if <=1.20.6 {
+                        /*.action((screen, option) -> Util.getPlatform().openUri(NaturallyTrimmed.getConfigPath().toUri()))
+                        *///?} else
                         .action((screen, option) -> Util.getPlatform().openPath(NaturallyTrimmed.getConfigPath()))
                         .build())
 
