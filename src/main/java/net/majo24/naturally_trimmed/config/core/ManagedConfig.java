d18a8477f0bbb164dd1b89ebdbc1a26b39537f48
majo24 <martin.j.schilling@gmx.de>
2026-08-06 13:42:28 +0200

    have gson fallback to field name instead of UpperCamelCase

--- src/main/java/net/majo24/naturally_trimmed/config/core/ManagedConfig.java
+++ src/main/java/net/majo24/naturally_trimmed/config/core/ManagedConfig.java
@@ -40,7 +40,6 @@ public abstract class ManagedConfig<T> {
         this.defaultsGetter = defaultsGetter;
 
         GsonBuilder builder = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
                 .serializeNulls()
                 .setPrettyPrinting();
         for (Map.Entry<Type, Object> typeAdapter : typeAdapters.entrySet()) {
